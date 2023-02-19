8ca1d8dc9e66bd664fffb2dc9611c0d9cc09467e
martinjatto <martinjatto1993@gmail.com>
2023-02-18 16:57:31 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alfa
+ * Return: 0
+ */
+int main(void)
+{
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
